36341eb8af87cf7023bc9bfc8adea4a661701165
venky252003 <venky25@gmail.com>
2026-03-31 10:55:37 +0530

    updated rag langchain

--- langchain/core_concept.py
+++ langchain/core_concept.py
@@ -6,6 +6,8 @@ from langchain_core import __version__ as langchain_core_version
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 print(f"LangChain Core Version: {langchain_core_version}")
 #print(f"LangGraph Version: {langgraph_version}")
@@ -54,6 +56,29 @@ def market_tagline_generator():
     tagline = chain.invoke({"product": product, "audience": audience})
     print(f"Generated Tagline: {tagline}")
 
+def new_chain():
+    llm = init_chat_model(model="qwen3.5:9b", model_provider="ollama", temperature=0.9)
+    prompt = ChatPromptTemplate.from_template("\
+                Your helpful assistant. Write short details on this {topic}. Total world should be less than 100 words.")
+    output_parser = StrOutputParser()
+
+    chain = prompt | llm | output_parser
+    topic = input("Enter a topic: ")
+    poem = chain.invoke({"topic": topic})
+    print(f"Generated Poem: {poem}")
+
+def demo_message_chain():
+    llm = init_chat_model(model="qwen3.5:9b", model_provider="ollama", temperature=0.9)
+    output_parser = StrOutputParser()
+
+    chain = [SystemMessage(content="You are a helpful assistant. Write short details on the given topic. Total world should be less than 100 words."),
+             HumanMessage(content="{topic}")]
+    
+    chain = ChatPromptTemplate.from_messages(chain) | llm | output_parser
+    topic = input("Enter a topic: ")
+    answer = chain.invoke({"topic": topic})
+    print(f"Answer: {answer}")
+
 def main():
     print("Hello, World!")  
 
@@ -61,5 +86,7 @@ if __name__ == "__main__":
     #demo_basic_chain()
     #demo_batch_execution()
     #demo_streaming_execution()
-    market_tagline_generator()
+    #market_tagline_generator()
+    #new_chain()
+    demo_message_chain()
 
